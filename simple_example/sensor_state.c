7fe52ad1d21b3a9de7dda206c31357ddcadc4d0c
Alex <giffenaa@vt.edu>
2020-02-05 17:51:35 -0500

    Added sensor state content

--- simple_example/sensor_state.c
+++ simple_example/sensor_state.c
@@ -2,7 +2,75 @@
  * sensor_state.c
  *
  *  Created on: Feb 4, 2020
- *      Author: trieu
+ *      Author: trieu, giffen
  */
 
+#include "sensor_state.h"
 
+/*
+ *  @function   updateFSM
+ *              Changes the state of the FSM based on the current
+ *              sensor value and timer inc.
+ *
+ *  @params     timeInc, sensorVal
+ */
+int updateFSM(struct fsmData fsm, int timeInc, int sensorVal) {
+
+    switch (fsm.curState) {
+        case Init:
+            fsm.curTime = 0;
+            fsm.sensorTotal = 0;
+            fsm.sensorCount = 0;
+            fsm.sensorAvg = 0;
+            fsm.curState = WaitingForTime1;
+            break;
+
+        case WaitingForTime1:
+            if (timeInc == 0) {
+                fsm.sensorTotal += sensorVal;
+                fsm.sensorCount++;
+            }
+            if (timeInc > 0) {
+                fsm.curTime += timeInc;
+                fsm.sensorAvg = fsm.sensorTotal/fsm.sensorCount;
+                // print uart sensor =
+                fsm.sensorTotal = 0;
+                fsm.sensorCount = 0;
+                fsm.curState = WaitingForTime2;
+            }
+            break;
+
+        case WaitingForTime2:
+            if (timeInc == 0) {
+                fsm.sensorTotal += sensorVal;
+                fsm.sensorCount++;
+            }
+            if (timeInc > 0) {
+                fsm.curTime += timeInc;
+                fsm.sensorAvg = fsm.sensorTotal/fsm.sensorCount;
+                // print uart sensor=
+                fsm.sensorTotal = 0;
+                fsm.sensorCount = 0;
+                fsm.curState = WaitingForTime3;
+            }
+            break;
+
+        case WaitingForTime3:
+            if (timeInc == 0) {
+                fsm.sensorTotal += sensorVal;
+                fsm.sensorCount++;
+            }
+            if (timeInc > 0) {
+                fsm.curTime += timeInc;
+                fsm.sensorAvg = fsm.sensorTotal/fsm.sensorCount;
+                // print uart sensor =
+                // print uart time =
+                fsm.sensorTotal = 0;
+                fsm.sensorCount = 0;
+                fsm.curState = WaitingForTime1;
+            }
+            break;
+    }
+
+    return 1;
+}
