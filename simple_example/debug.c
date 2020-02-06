f04b0dcb310aa06ab080e6bb76702fea390969b6
Joseph Chen <jchen20@vt.edu>
2020-02-05 22:04:01 -0500

    gpio debug

--- simple_example/debug.c
+++ simple_example/debug.c
@@ -1,9 +1,42 @@
 #include "debug.h"
 
-
 void debug_setup()
 {
     GPIO_init();
+    GPIO_setConfig(CONFIG_GPIO_0, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_1, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_2, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_3, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_4, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_5, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_6, GPIO_CFG_OUTPUT);
+    GPIO_setConfig(CONFIG_GPIO_7, GPIO_CFG_OUTPUT);
+
+    GPIO_write(CONFIG_GPIO_0, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_1, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_2, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_3, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_4, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_5, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_6, GPIO_CFG_OUT_HIGH);
+    GPIO_write(CONFIG_GPIO_7, GPIO_CFG_OUT_HIGH);
+}
+
+void dbgOutputLoc(unsigned int outLoc)
+{
+    if (outLoc > 0x7F)
+        stop_all();
+    
+    static bool firstbit = false;
+    firstbit = !firstbit;
+    GPIO_write(CONFIG_GPIO_0, firstbit ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_1, outLoc & 0x01 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_2, outLoc & 0x02 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_3, outLoc & 0x04 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_4, outLoc & 0x08 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_5, outLoc & 0x10 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_6, outLoc & 0x20 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_7, outLoc & 0x40 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
 }
 
 void dbgUARTVal(unsigned char outVal)
