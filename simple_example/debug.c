c137a76dfbec791e29d23cf520a367ee7f3e0e78
Stevent7 <stevent7@vt.edu>
2020-02-13 11:54:37 -0500

    Added task suspending and changed interrupt disables to critical section

--- simple_example/debug.c
+++ simple_example/debug.c
@@ -57,9 +57,8 @@ void dbgOutputLoc(unsigned int outLoc)
     if (outLoc > 0x7B)
         stop_all(FAIL_UNKNOWN_CODE);
     
-    static bool firstbit = false;
-    firstbit = !firstbit;
-    GPIO_write(CONFIG_GPIO_0, firstbit ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+    GPIO_write(CONFIG_GPIO_0, GPIO_CFG_OUT_HIGH);
+
     GPIO_write(CONFIG_GPIO_1, outLoc & 0x01 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
     GPIO_write(CONFIG_GPIO_2, outLoc & 0x02 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
     GPIO_write(CONFIG_GPIO_3, outLoc & 0x04 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
@@ -67,6 +66,9 @@ void dbgOutputLoc(unsigned int outLoc)
     GPIO_write(CONFIG_GPIO_5, outLoc & 0x10 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
     GPIO_write(CONFIG_GPIO_6, outLoc & 0x20 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
     GPIO_write(CONFIG_GPIO_7, outLoc & 0x40 ? GPIO_CFG_OUT_HIGH : GPIO_CFG_OUT_LOW);
+
+    // state change
+    GPIO_write(CONFIG_GPIO_0, GPIO_CFG_OUT_LOW);
 }
 
 void dbgUARTVal(unsigned char outVal)
@@ -88,7 +90,9 @@ void dbgUARTVal(unsigned char outVal)
 
 void stop_all(unsigned int FAILURE_CODE)
 {
-    //taskDISABLE_INTERRUPTS();
+    vTaskSuspendAll();
+    taskENTER_CRITICAL();
+
     GPIO_setConfig(CONFIG_GPIO_LED_0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
     GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON);
 
@@ -107,6 +111,8 @@ void stop_all(unsigned int FAILURE_CODE)
         int i = 0;
         for (;i<100000;i++) ;
     }
+
+    taskEXIT_CRITICAL();
 }
 
 
