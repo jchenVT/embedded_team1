954b858b0f39daadd18856dda6e8e0a156ea7d00
Alex <giffenaa@vt.edu>
2020-02-23 16:57:39 -0500

    Changed documentation and unfinished thread usage

--- simple_example/main_freertos.c
+++ simple_example/main_freertos.c
@@ -1,38 +1,3 @@
-/*
- * Copyright (c) 2016-2019, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- *  ======== main_freertos.c ========
- */
 #include <stdint.h>
 
 #ifdef __ICCARM__
@@ -56,7 +21,7 @@
 
 /* Get thread locations */
 extern void *readProximity1Thread(void *arg0);
-extern void *readProximity2Thread(void *arg0);
+//extern void *readProximity2Thread(void *arg0);
 
 /* Stack size in bytes */
 #define THREADSTACKSIZE   1024
@@ -66,34 +31,35 @@ extern void *readProximity2Thread(void *arg0);
  */
 int main(void)
 {
+    /* UART0 Proximity Sensor Read */
     pthread_t           thread1;
-    pthread_t           thread2;
     pthread_attr_t      attrs1;
-    pthread_attr_t      attrs2;
     int                 retcProx1;
-    int                 retcProx2;
+
+    //pthread_t           thread2;
+    //pthread_attr_t      attrs2;
+    //int                 retcProx2;
 
     /* initialize the system locks */
-#ifdef __ICCARM__
-    __iar_Initlocks();
-#endif
+    #ifdef __ICCARM__
+        __iar_Initlocks();
+    #endif
 
-    /* Call driver init functions */
+    /* Call driver setup functions */
     Board_init();
-    debug_setup();
-
+    //debug_setup();
     if (!setupQs()) {
         //stop_all(FAIL_Q1_INIT);
     }
 
     /* Initialize the attributes structure with default values */
     pthread_attr_init(&attrs1);
-    pthread_attr_init(&attrs2);
+    //pthread_attr_init(&attrs2);
 
     retcProx1 = pthread_create(&thread1, &attrs1, readProximity1Thread, NULL);
-    retcProx2 = pthread_create(&thread2, &attrs2, readProximity2Thread, NULL);
+    //retcProx2 = pthread_create(&thread2, &attrs2, readProximity2Thread, NULL);
 
-    if (retcProx1 != 0 && retcProx2 != 0) {
+    if (retcProx1 != 0) {
         /* pthread_create() failed */
         while (1) {}
     }
@@ -104,36 +70,27 @@ int main(void)
     return (0);
 }
 
-//*****************************************************************************
-//
-//! \brief Application defined malloc failed hook
-//!
-//! \param  none
-//!
-//! \return none
-//!
-//*****************************************************************************
-void vApplicationMallocFailedHook()
-{
+/**
+ *  vApplicationMallocFailedHook()
+ *      Application defined malloc failed hook
+ */
+void vApplicationMallocFailedHook() {
+
     /* Handle Memory Allocation Errors */
-    while(1)
-    {
+    while(1) {
+
     }
 }
 
-//*****************************************************************************
-//
-//! \brief Application defined stack overflow hook
-//!
-//! \param  none
-//!
-//! \return none
-//!
-//*****************************************************************************
-void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
-{
-    //Handle FreeRTOS Stack Overflow
-    while(1)
-    {
+
+/**
+ *  vApplicationStackOverflowHook()
+ *      Application defined stack overflow hook
+ */
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
+
+    /* Handle FreeRTOS Stack Overflow */
+    while(1) {
+
     }
 }
