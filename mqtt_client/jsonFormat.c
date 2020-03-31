8c8c7f10cc32d78e626ca2fb5ffb1287a86d742f
Christina Lin <christie@vt.edu>
2020-03-31 00:36:14 -0400

    changed hardcoded stuffs

--- mqtt_client/jsonFormat.c
+++ mqtt_client/jsonFormat.c
@@ -4,6 +4,7 @@
 #include <jsmn.h>
 #include <string.h>
 
+#define JSON_LEN 120
 jsmn_parser parser;
 
 int jsonParser(char * jsonMsg) {
@@ -46,29 +47,29 @@ int jsonParser(char * jsonMsg) {
 }
 
 int packageArmJSON(int state) {
-    char json[20];
-    snprintf(json, 20, "{ \"state\": %d }", state);
+    char json[JSON_LEN];
+    snprintf(json, JSON_LEN, "{ \"state\": %d }", state);
 //    return sendToPubQ(0, &json);
     return 1;
 }
 
 int packageArmSensorJSON(int sensorID, int sensorValue) {
-    char json[50];
-    snprintf(json, 50, "{ \"sensorID\": %d, \"sensorValue\": %d }", sensorID, sensorValue);
+    char json[JSON_LEN];
+    snprintf(json, JSON_LEN, "{ \"sensorID\": %d, \"sensorValue\": %d }", sensorID, sensorValue);
 //    return sendToPubQ(1, &json);
     return 1;
 }
 
 int packageRoverJSON(int state) {
-    char json[20];
-    snprintf(json, 20, "{ \"state\": %d }", state);
+    char json[JSON_LEN];
+    snprintf(json, JSON_LEN, "{ \"state\": %d }", state);
 //    return sendToPubQ(2, &json);
     return 1;
 }
 
 int packageRoverSensorJSON(bool move_to_point, int point_x, int point_y, int angle_rotate) {
-    char json[110];
-    snprintf(json, 120, "{ \"move_to_point\": %s, \"point_x\": %d, \"point_y\": %d, \"angle_rotate\": %d }", move_to_point ? "true"; "false", point_x, point_y, angle_rotate);
+    char json[JSON_LEN];
+    snprintf(json, JSON_LEN, "{ \"move_to_point\": %s, \"point_x\": %d, \"point_y\": %d, \"angle_rotate\": %d }", move_to_point ? "true"; "false", point_x, point_y, angle_rotate);
 //    return sendToPubQ(3, &json);
     return 1;
 }
