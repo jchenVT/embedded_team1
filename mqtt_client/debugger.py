12a7486632d0650d59cf8aaa342f9562532854d9
jchenvt <jchen20@vt.edu>
2020-04-02 09:46:18 -0400

    finalize debugger with stats

--- mqtt_client/debugger.py
+++ mqtt_client/debugger.py
@@ -3,6 +3,9 @@ import argparse
 import json
 
 topics = {}
+topics_num = {}
+topics_msgs = {}
+topics_time_last = {}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
@@ -11,37 +14,61 @@ with open(args.filename, "r") as file:
     for line in file:
         line_list = line.split()
         topics.update({line_list[0] : line_list[1:]})
+        topics_num.update({line_list[0] : (0,0)})
+        topics_msgs.update({line_list[0] : 0})
 
 def on_connect(client, userdata, flags, rc):
     print("connected")
     for key in topics:
         client.subscribe(key)
+    client.subscribe('debug')
 
 def on_message(client, userdata, msg):
-    print ('received')
-    fields = topics.get(msg.topic)
+    # try reading json
     try:
         msg_dict = json.loads(msg.payload)
     except:
-        print('Impropert json format!')
+        print('Improper json format!')
         client.publish(f'{msg.topic}/debug', json.dumps({'success': False, 'error' : 'improper json'}))
-    missing_fields = []
-    for field in fields:
-        if msg_dict.get(field) is None:
-            missing_fields.append(field) 
-        else:
-            continue
 
-    if len(missing_fields) > 0:
-        print("some missing")
-        try:
-            client.publish(f'{msg.topic}/debug', json.dumps({'success': False, 'error' : 'missing field', 'missing': ' ,'.join(missing_fields)}))
-        except:
-            print('publish fail')
-            return
+    # if debug, update 
+    if msg.topic == 'debug':
+        attempted, received = msg_dict['attemptPubCount'], msg_dict['recvSubCount']
+        topics_num.update({msg.topic : (attempted, received)}) 
+        subTopic = msg_dict['subTopic'] 
+        pubTopic = msg_dict['pubTopic'] 
+        print("==========================================")
+        print(f'From {pubTopic}, status: {msg_dict["status"]}')
+        print(f'{pubTopic}: attempted - {attempted}, recorded = {topics_msgs.get(pubTopic)}')
+        print(f'{subTopic}: received  - {received }, recorded = {topics_msgs.get(subTopic)}')
+
+        if attempted != topics_msgs.get(pubTopic):
+            topics_msgs.update({pubTopic : attempted})
+        if received != topics_msgs.get(pubTopic):
+            topics_msgs.update({subTopic : received})
+
     else:
-        print("none missing")
-        client.publish(f'{msg.topic}/debug', json.dumps({'success': True}))
+        count = topics_msgs.get(msg.topic) 
+        count += 1
+        topics_msgs.update({msg.topic : count}) 
+        fields = topics.get(msg.topic)
+        missing_fields = []
+        for field in fields:
+            if msg_dict.get(field) is None:
+                missing_fields.append(field) 
+            else:
+                continue
+
+        if len(missing_fields) > 0:
+            print(f"{msg.topic}: some topics missing")
+            try:
+                client.publish(f'{msg.topic}/debug', json.dumps({'success': False, 'error' : 'missing field', 'missing': ' ,'.join(missing_fields)}))
+            except:
+                print('publish fail')
+                return
+        else:
+            # print(f"{msg.topic}: all fields found")
+            client.publish(f'{msg.topic}/debug', json.dumps({'success': True}))
 
 
 
